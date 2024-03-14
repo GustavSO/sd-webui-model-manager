4f87216e7cc9841cc9cf768948e30c94eae14d6b
GustavSO <guol@itu.dk>
2024-03-14 22:13:49 +0100

    Improved Settings Menu
    - Created a separate category for settings
    - Included more detailed information for each setting

--- scripts/model_manager.py
+++ scripts/model_manager.py
@@ -1,5 +1,7 @@
+from sympy import true
 import modules.scripts as scripts
 from modules import script_callbacks, shared
+from modules.options import categories
 
 import gradio as gr
 from scripts.mm_libs import loader
@@ -21,7 +23,6 @@ def on_ui_tabs():
         # with gr.Tab("Utilities"):
         #     utilities.UI()
 
-
     loader.sort_dirs()
     return [(ui_component, "Model Manager", "model_manager_tab")]
 
@@ -30,23 +31,64 @@ def on_ui_tabs():
 # Settings Menu #
 #################
 def on_ui_settings():
-    MM_SECTION = ("mm", "Model Manager")
+    categories.register_category("mm", "Model Manager")
+    cat_id = "mm"
 
+    # Sections
+    download = ("mm_download", "Download")
+
+    if not (hasattr(shared.OptionInfo, "info") and callable(getattr(shared.OptionInfo, "info"))):
+        def info(self, info):
+            self.label += f" ({info})"
+            return self
+        shared.OptionInfo.info = info
+
+    
     mm_options = {
-        "mm_auto_paste": shared.OptionInfo(True, "Enable Auto-paste"),
-        "mm_allow_NSFW": shared.OptionInfo(True, "Allow NSFW Images"),
-        "mm_civitai_api_key": shared.OptionInfo("", 'Civitai API Key used to download certain models. Create one at https://civitai.com/user/account'),
-        "mm_supress_API_warnings": shared.OptionInfo(False, "Supress API Key warnings on fetch. Will still show errors on download if it fails due to authentication issues."),
-        "mm_folder_depth": shared.OptionInfo(1, "Depth of subdirectories to search for when downloading models. Default is 1, which means it will only search the immediate subdirectories of the root folder for the model type.", gr.Slider,
-        {"minimum": 0, "maximum": 10, "step": 1}),
-        "mm_github_token": shared.OptionInfo("", 'The GitHub Token used to create a Selenium session. Feature still in development.'),
+        "mm_auto_paste": shared.OptionInfo(
+            True, 
+            "Enable Auto-paste",
+            section=download,
+            ).info("Will automatically paste the clipboard contents into the URL input field when fetching models"),
+
+        "mm_allow_NSFW": shared.OptionInfo(
+            True,
+            "Allow NSFW Images",
+            section=download,
+            ).info("Will display NSFW images when fetching models"),
+
+        "mm_civitai_api_key": shared.OptionInfo(
+            "",
+            "Civitai API key",
+            section=download,
+        ).info("API Key used to download certain models. Create one at https://civitai.com/user/account"),
+
+        "mm_supress_API_warnings": shared.OptionInfo(
+            False,
+            "Supress API key warning",
+            section=download,
+        ).info("Only applies when fetching. Will still show errors on download if it fails due to authentication issues"),
+
+        "mm_folder_depth": shared.OptionInfo(
+            1,
+            "Subdirectory Depth",
+            gr.Slider,
+            {"minimum": 0, "maximum": 10, "step": 1},
+            section=download,
+        ).info("This is the depth of subdirectories to search for when downloading models. Default is 1, which means it will only search the immediate subdirectories of the root folder for the model type"),
+
+        "mm_github_token": shared.OptionInfo(
+            "",
+            "GitHub Token",
+            section=download,
+        ).info("GitHub Token used to create a Selenium session, for fetching civitai notifications. Feature still in development!"),
     }
 
+    # TODO: When more sections are added, assign it here instead
     for key, opt in mm_options.items():
-        opt.section = MM_SECTION
+        opt.category_id = cat_id
         shared.opts.add_option(key, opt)
 
 
 script_callbacks.on_ui_settings(on_ui_settings)
 script_callbacks.on_ui_tabs(on_ui_tabs)
-
