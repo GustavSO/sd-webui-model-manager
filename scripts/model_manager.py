2a36819954e929bdc9b4692cb8f7b81934034b37
GustavSO <guol@itu.dk>
2024-03-26 20:00:47 +0100

    "Auto Naming" settings section
    - Moved naming settings to their own section called "Auto Naming"
    - Renamed "Development" section to "Testing" to ensure that it is ordered lower than the other sections

--- scripts/model_manager.py
+++ scripts/model_manager.py
@@ -33,12 +33,17 @@ def on_ui_settings():
 
     categories.register_category("mm", "Model Manager")
 
+
+
     options_templates.update(options_section(('mm_download', "Download", "mm"), {
         "mm_auto_paste": OptionInfo(True, "Auto-paste clipboard").info("Will automatically paste the clipboard contents into the URL input field when fetching models"),
         "mm_allow_NSFW": OptionInfo(True, "Allow NSFW Images").info("Will display NSFW images when fetching models"),
         "mm_civitai_api_key": OptionInfo("", "Civitai API key").info("API Key used to download certain models. Create one at https://civitai.com/user/account"),
         "mm_supress_API_warnings": OptionInfo(False, "Supress API key warning").info("Only applies when fetching. Will still show errors on download if it fails due to authentication issues"),
         "mm_folder_depth": OptionInfo(1, "Subdirectory Depth", gr.Slider, {"minimum": 0, "maximum": 10, "step": 1}).info("This is the depth of subdirectories to search for when downloading models. Default is 1, which means it will only search the immediate subdirectories of the root folder for the model type"),
+    }))
+
+    options_templates.update(options_section(('mm_auto_naming', "Auto Naming", "mm"), {
         "mm_auto_naming_formatting": OptionInfo("model_name model_version [model_base] (model_creator)", "Naming Format").js("Info", "settingsHints").info("The formatting of the auto naming suggestion. Need to adhere to filenaming restriction of the OS, eg. no / or \ in Windows. If left empty it will default to the name of the model as it is displayed on the model page"),
         "mm_auto_trim_whitespace": OptionInfo(True, "Trim Whitespace").info("Automatically trim consecutive whitespace series to a single space in the filename during model downloads"),
         "mm_auto_trim_illegal_chars": OptionInfo(True, "Trim Illegal Characters").info("Automatically remove any illegal characters from the filename when downloading a model"),
@@ -50,11 +55,11 @@ def on_ui_settings():
         "mm_filter_alphabet": OptionInfo([], "Filter Alphabets", ui_components.DropdownMulti, lambda: {"choices": list(["Chinese", "Japanese", "Latin", "Cyrillic"])}).info("Automatically remove characters from the selected alphabets from the filename when downloading a model"),
         "mm_excluded_words_or_phrases": OptionInfo("", "Excluded Words or Phrases").info("Words or phrases to exclude from the filename. Only done once when fetching and only effects model_name or model_version. Useful for removing redundant words like lora, embedding etc. Separate words with a comma (,). Eg. 'lora, embedding, model'. Case insensitive"),
         "mm_format_on_fetch": OptionInfo(True, "Format on Fetch").info("Automatically format the filename when fetching a model in adherence to the formatting settings. If disabled, the filename will be formatted when downloading the model instead"),
-        "mm_github_token": OptionInfo("", "GitHub Token").info("GitHub Token used to create a Selenium session, for fetching civitai notifications. Feature still in development!")
-    }))
+        }))
 
-    options_templates.update(options_section(('mm_dev', "Development", "mm"), {
+    options_templates.update(options_section(('mm_test', "Testing", "mm"), {
         "mm_disable_download": OptionInfo(False, "Disable Download").info("Disables the downloading, useful for debugging and testing the UI without actually downloading anything"),
+        "mm_github_token": OptionInfo("", "GitHub Token").info("GitHub Token used to create a Selenium session, for fetching civitai notifications. Feature still in development!")
         }))
 
     # TODO: When more sections are added, assign it here instead
