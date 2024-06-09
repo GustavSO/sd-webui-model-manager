d50d39fdc64c256dee57d56cc1ad959069559fd5
GustavSO <guol@itu.dk>
2024-06-09 18:52:46 +0200

    Changed card svg icon

--- javascript/mm.js
+++ javascript/mm.js
@@ -171,6 +171,17 @@ function addSVGPaths(svgElement, paths) {
   });
 }
 
+function addSVGRects(svgElement, rects) {
+  rects.forEach((rectData) => {
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("x", rectData[0]);
+    rect.setAttribute("y", rectData[1]);
+    rect.setAttribute("width", rectData[2]);
+    rect.setAttribute("height", rectData[3]);
+    svgElement.appendChild(rect);
+  });
+}
+
 async function openModelOnCivitai(jsonFile) {
   sendMessageToGradio(jsonFile);
 
@@ -190,19 +201,43 @@ async function openModelOnCivitai(jsonFile) {
   }
 }
 
-function cardSvgIcon() {
+function civitSvgIcon() {
   const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  const paths = [
+  const civit_path = [
     "M 352.79 218.85 L 319.617 162.309 L 203.704 162.479 L 146.28 259.066 L 203.434 355.786 L 319.373 355.729 L 352.773 299.386 L 411.969 299.471 L 348.861 404.911 L 174.065 404.978 L 87.368 259.217 L 174.013 113.246 L 349.147 113.19 L 411.852 218.782 L 352.79 218.85 Z",
-    "M 304.771 334.364 L 213.9 334.429 L 169.607 259.146 L 214.095 183.864 L 305.132 183.907 L 330.489 227.825 L 311.786 259.115 L 330.315 290.655 Z M 278.045 290.682 L 259.294 259.18 L 278.106 227.488 L 240.603 227.366 L 221.983 259.128 L 240.451 291.026 Z",
   ];
 
   svgIcon.setAttribute("width", "1.8rem");
   svgIcon.setAttribute("height", "1.8rem");
-  svgIcon.setAttribute("viewBox", "75 85 350 350");
+  svgIcon.setAttribute("viewBox", "87.368 113.19 324.484 291.721");
   svgIcon.setAttribute("fill", "white");
-  // svgElement.style.marginTop = size === "1.8rem" ? "-2px" : "0";
-  addSVGPaths(svgIcon, paths);
+  addSVGPaths(svgIcon, civit_path);
+
+  return svgIcon;
+}
+
+function cardSvgIcon() {
+  const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  const path = [
+    "M 102.618 205.229 c -56.585 0 -102.616 -46.031 -102.616 -102.616 C 0.002 46.031 46.033 0 102.618 0 C 159.2 0 205.227 46.031 205.227 102.613 C 205.227 159.198 159.2 205.229 102.618 205.229 Z M 102.618 8.618 c -51.829 0 -94.002 42.166 -94.002 93.995 s 42.17 93.995 94.002 93.995 c 51.825 0 93.988 -42.162 93.988 -93.995 C 196.606 50.784 154.444 8.618 102.618 8.618 Z",
+    "M 104.941 62.111 c -48.644 0 -84.94 -10.704 -87.199 -11.388 l 2.494 -8.253 c 0.816 0.247 82.657 24.336 164.38 -0.004 l 2.452 8.26 C 158.405 59.266 130.021 62.111 104.941 62.111 Z",
+    "M 20.416 160.572 l -2.459 -8.26 c 84.271 -25.081 165.898 -1.027 169.333 0 l -2.494 8.256 C 183.976 160.318 102.142 136.24 20.416 160.572 Z",
+    "M 69.399 196.168 C 26.933 96.747 63.584 8.604 63.959 7.727 l 7.927 3.378 c -0.365 0.845 -35.534 85.756 5.44 181.677 L 69.399 196.168 Z",
+    "M 135.168 196.168 l -7.927 -3.382 c 40.971 -95.92 5.801 -180.832 5.436 -181.677 l 7.927 -3.378 C 140.973 8.604 177.627 96.747 135.168 196.168 Z",
+  ];
+
+  const rects = [
+    [5.746, 98.304, 193.734, 8.618],
+    [98.304, 5.746, 8.618, 193.734],
+  ];
+
+  svgIcon.setAttribute("width", "1.8rem");
+  svgIcon.setAttribute("height", "1.8rem");
+  svgIcon.setAttribute("viewBox", "-20.52 -20.52 246.27 246.27");
+  svgIcon.setAttribute("fill", "white");
+  addSVGPaths(svgIcon, path);
+  addSVGRects(svgIcon, rects);
 
   return svgIcon;
 }
